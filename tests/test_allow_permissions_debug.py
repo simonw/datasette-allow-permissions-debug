a54b36bc37b8e537213ee9f52edfb6e26ce67412
Simon Willison <swillison@gmail.com>
2025-10-30 12:09:47 -0700

    Switch to ds.client for tests

--- tests/test_allow_permissions_debug.py
+++ tests/test_allow_permissions_debug.py
@@ -5,17 +5,15 @@ import httpx
 
 @pytest.mark.asyncio
 async def test_plugin_is_installed():
-    app = Datasette([], memory=True).app()
-    async with httpx.AsyncClient(app=app) as client:
-        response = await client.get("http://localhost/-/plugins.json")
-        assert 200 == response.status_code
-        installed_plugins = {p["name"] for p in response.json()}
-        assert "datasette-allow-permissions-debug" in installed_plugins
+    ds = Datasette([], memory=True)
+    response = await ds.client.get("/-/plugins.json")
+    assert 200 == response.status_code
+    installed_plugins = {p["name"] for p in response.json()}
+    assert "datasette-allow-permissions-debug" in installed_plugins
 
 
 @pytest.mark.asyncio
 async def test_allows_access():
-    app = Datasette([], memory=True).app()
-    async with httpx.AsyncClient(app=app) as client:
-        response = await client.get("http://localhost/-/permissions")
-        assert 200 == response.status_code
+    ds = Datasette([], memory=True)
+    response = await ds.client.get("/-/permissions")
+    assert 200 == response.status_code
